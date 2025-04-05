9a5ce1266f78208186fca0154b18819a7cade67f
Akash <akash.todewale99@gmail.com>
2025-04-05 13:20:11 +0530

    updated from personal

--- src/test/java/com/ellora/pageObjects/ReportsPage.java
+++ src/test/java/com/ellora/pageObjects/ReportsPage.java
@@ -10,45 +10,45 @@ import org.openqa.selenium.interactions.Actions;
 
 import com.ellora.Utilities.WaitUtil;
 public class ReportsPage {
-   private WebDriver driver;
+   public static WebDriver driver;
   
     public ReportsPage(WebDriver driver) {
 	super();
-	this.driver = driver;
-}
+	ReportsPage.driver = driver;
+    }
 
 	// Locators
-    private By reportsMenu = By.xpath("//a[normalize-space()='Reports']");
-    private By reportDropdown = By.xpath("//div[@data-tooltip=\"Select Report\"]");
-    private By searchReport = By.xpath("//div[@id=\"select_report_name\"]//input[@class=\"vscomp-search-input\"]");
-    private By includeInactiveButton = By.xpath("//input[@id=\"report_inactive_provider\"]");
-    private By providerDropdown = By.xpath("//div[contains(text(),'Select Provider')]/parent::div/parent::div");
-    private By searchProvider = By.xpath("//div[@id=\"report_select_providers\"]//input[@class=\"vscomp-search-input\"]");
-    private By facilityDropdown = By.xpath("//div[contains(text(),'Select Facility')]");
-    private By searchFacility = By.xpath("//div[@id=\"report_select_facilities\"]//input[@class=\"vscomp-search-input\"]");
-    private By clientDropdown = By.xpath("//div[@class=\"vscomp-toggle-button\" and normalize-space()=\"Select Client\"]");
-    private By selectAllClients = By.xpath("(//span[@class=\"vscomp-toggle-all-button\"])[2]");
-    private By serviceDateField = By.xpath("//input[@id=\"report_date_range\"]/following-sibling::input");
-    private By getReportButton = By.xpath("//button[normalize-space()='Get Report']");
-    private By sendEmailField = By.xpath("//input[@id=\"report_send_email\"]");
-    private By sendEmailButton = By.xpath("//button[@id=\"report_send_email_button\"]");
-    private By emailSuccessMessage = By.xpath("//div[@class='toast-body fw-normal' and normalize-space()=\"Request for exporting report is accepted. You should receive an email once the report is ready!\"]");
+    public static final By reportsMenu = By.xpath("//a[normalize-space()='Reports']");
+    public static final By reportDropdown = By.xpath("//div[@data-tooltip=\"Select Report\"]");
+    public static final By searchReport = By.xpath("//div[@id=\"select_report_name\"]//input[@class=\"vscomp-search-input\"]");
+    public static final By includeInactiveButton = By.xpath("//input[@id=\"report_inactive_provider\"]");
+    public static final By providerDropdown = By.xpath("//div[contains(text(),'Select Provider')]/parent::div/parent::div");
+    public static final By searchProvider = By.xpath("//div[@id=\"report_select_providers\"]//input[@class=\"vscomp-search-input\"]");
+    public static final By facilityDropdown = By.xpath("//div[contains(text(),'Select Facility')]");
+    public static final By searchFacility = By.xpath("//div[@id=\"report_select_facilities\"]//input[@class=\"vscomp-search-input\"]");
+    public static final By clientDropdown = By.xpath("//div[@class=\"vscomp-toggle-button\" and normalize-space()=\"Select Client\"]");
+    public static final By selectAllClients = By.xpath("(//span[@class=\"vscomp-toggle-all-button\"])[2]");
+    public static final By serviceDateField = By.xpath("//input[@id=\"report_date_range\"]/following-sibling::input");
+    public static final By getReportButton = By.xpath("//button[normalize-space()='Get Report']");
+    public static final By sendEmailField = By.xpath("//input[@id=\"report_send_email\"]");
+    public static final By sendEmailButton = By.xpath("//button[@id=\"report_send_email_button\"]");
+    public static final By emailSuccessMessage = By.xpath("//div[@class='toast-body fw-normal' and normalize-space()=\"Request for exporting report is accepted. You should receive an email once the report is ready!\"]");
 
     // Constructor
   
 
     // Methods
-    public void openReports() {
+    public static void openReports() {
         driver.findElement(reportsMenu).click();
     }
 
-    public void selectReport(String reportName) {
+    public static void selectReport(String reportName) {
         driver.findElement(reportDropdown).click();
         driver.findElement(searchReport).sendKeys(reportName);
         driver.findElement(searchReport).sendKeys(Keys.ENTER);
     }
 
-    public void filterReport(String provider, String facility, String serviceDate) throws InterruptedException {
+    public static void filterReport(String provider, String facility, String serviceDate) throws InterruptedException {
     	 Actions actions = new Actions(driver);
         driver.findElement(includeInactiveButton).click();
        
@@ -73,18 +73,19 @@ public class ReportsPage {
         driver.findElement(serviceDateField).sendKeys(Keys.ENTER);
     }
 
-    public void generateReport() {
+    public static void generateReport() {
         driver.findElement(getReportButton).click();
     }
 
-    public void sendEmailReport(String email) {
+    public static void sendEmailReport(String email) {
         if (driver.findElement(sendEmailField).isDisplayed()) {
             driver.findElement(sendEmailField).sendKeys(email);
             driver.findElement(sendEmailButton).click();
         }
     }
 
-    public boolean isEmailSent() {
+    public static boolean isEmailSent() {
         return driver.findElement(emailSuccessMessage).isDisplayed();
     }
+    
 }
--- src/test/java/com/ellora/testCases/ReportManage_appt_note.java
+++ src/test/java/com/ellora/testCases/ReportManage_appt_note.java
@@ -10,41 +10,46 @@ import org.testng.annotations.Test;
 import com.ellora.pageObjects.LoginPage;
 import com.ellora.pageObjects.ReportsPage;
 
-public class ReportManage_appt_note {
-	public WebDriver driver;
+public class ReportManage_appt_note extends BaseClass{
+	//public WebDriver driver;
 
 	@Test
-	public void testReportsGeneration() throws InterruptedException {
+	public void testReportsGeneration() {
 
-		driver = BaseClass.initializeDriver("chrome");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("https://schedulerstage1mentalhealth.ellorasystems.com/");
-		LoginPage loginPage = new LoginPage(driver);
-
-		ReportsPage reportsPage = new ReportsPage(driver);
-
-		// Perform login steps
-		loginPage.enterUsername("medicalstage1");
-		loginPage.enterPassword("Bb123!@#");
-		loginPage.clickLoginButton();
+//		driver = BaseClass.initializeDriver("chrome");
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		driver.get("https://schedulerstage1mentalhealth.ellorasystems.com/");
+//		LoginPage loginPage = new LoginPage(driver);
+//
+//		ReportsPage reportsPage = new ReportsPage(driver);
+//
+//		// Perform login steps
+//		loginPage.enterUsername("medicalstage1");
+//		loginPage.enterPassword("Bb123!@#");
+//		loginPage.clickLoginButton();
 
 		// Open Reports
-		reportsPage.openReports();
+		ReportsPage.openReports();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		// Select Report
-		reportsPage.selectReport("Brief Note Report");
+		ReportsPage.selectReport("Brief Note Report");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		// Apply Filters
-		reportsPage.filterReport("Sam Doc", "Stage1 Mental Health Service", "Jan 01, 2025 to Mar 10, 2025");
+		try {
+			ReportsPage.filterReport("Sam Doc", "Omni", "Jan 01, 2025 to Mar 10, 2025");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		// Generate Report
-		reportsPage.generateReport();
+		ReportsPage.generateReport();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		// Send Email Report
-		reportsPage.sendEmailReport("akash.todewale@bostonbyte.com");
+		ReportsPage.sendEmailReport("akash.todewale@bostonbyte.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		// Verify Email Sent
-		Assert.assertTrue(reportsPage.isEmailSent(), "Email was not sent successfully!");
+		Assert.assertTrue(ReportsPage.isEmailSent(), "Email was not sent successfully!");
 	}
 }
